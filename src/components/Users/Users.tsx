b3ff97bee741648cd6317a2d44f05aa66897667d
Danila Serpokrylov <serpokrylov.danila@mail.ru>
2021-08-23 23:03:05 +0300

    added function of saving uri parameters when page is reloading

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import Paginator from '../common/Paginator/Paginator';
 import User from "../../components/Users/User";
 import { UsersSearchForm } from './UsersSearchForm';
@@ -12,11 +12,11 @@ import {
     getUsers,
     getUsersFilter
 } from '../../redux/users-selectors';
+import { useHistory } from 'react-router-dom';
+import * as queryString from 'querystring';
 
-type PropsType = {}
-
-
-export const Users: FC<PropsType> = (props) => {
+type QueryParamsType = { term?: string, page?: string, friend?: string };
+export const Users: React.FC = (props) => {
     const totalUsersCount = useSelector(getTotalUsersCount)
     const currentPage = useSelector(getCurrentPage)
     const pageSize = useSelector(getPageSize)
@@ -37,8 +37,38 @@ export const Users: FC<PropsType> = (props) => {
         dispatch(follow(userId))
     }
     useEffect(() => {
-        dispatch(requestUsers(currentPage, pageSize, filter))
+        const parsed = queryString.parse(history.location.search.substr(1)) as QueryParamsType
+
+        let actualPage = currentPage
+        let actualFilter = filter
+        if (!!parsed.page) actualPage = Number(parsed.page)
+        if (!!parsed.term) actualFilter = { ...actualFilter, term: parsed.term as string }
+        switch (parsed.friend) {
+            case "null":
+                actualFilter = { ...actualFilter, friend: null }
+                break
+            case "true":
+                actualFilter = { ...actualFilter, friend: true }
+                break
+            case "false":
+                actualFilter = { ...actualFilter, friend: false }
+                break
+        }
+        dispatch(requestUsers(actualPage, pageSize, actualFilter))
     }, [])
+    useEffect(() => {
+        const query: QueryParamsType = {}
+
+        if (!!filter.term) query.term = filter.term
+        if (filter.friend !== null) query.friend = String(filter.friend)
+        if (currentPage !== 1) query.page = String(currentPage)
+
+        history.push({
+            pathname: '/users',
+            search: queryString.stringify(query)
+        })
+    }, [filter, currentPage])
+    const history = useHistory();
     return <div>
         <UsersSearchForm onFilterChanged={onFilterChanged}/>
         <Paginator currentPage={currentPage} onPageChanged={onPageChanged}
--- src/components/Users/UsersSearchForm.tsx
+++ src/components/Users/UsersSearchForm.tsx
@@ -1,31 +1,36 @@
 import { Field, Form, Formik } from 'formik';
 import React from 'react';
 import { FilterType } from '../../redux/users-reducer';
+import { getUsersFilter } from '../../redux/users-selectors';
+import { useSelector } from 'react-redux';
 
 const usersSearchFormValidate = (values: any) => {
     const errors = {};
     return errors;
 }
 
+type FriendFormType = "true" | "false" | "null";
 type FormType = {
     term: string,
-    friend: "true" | "false" | "null"
+    friend: FriendFormType
 }
 
 type PropsType={
     onFilterChanged: (filter: FilterType) => void
 }
 export const UsersSearchForm:React.FC<PropsType> = React.memo((props) => {
+    const filter = useSelector(getUsersFilter)
+
     const submit = (values: FormType, { setSubmitting }: { setSubmitting: (isSubmitting: boolean) => void }) => {
         const filter:FilterType = {term: values.term,
         friend: values.friend === "null" ? null : values.friend === "true" ? true : false}
-
         props.onFilterChanged(filter)
         setSubmitting(false)
     }
     return <div>
         <Formik
-            initialValues={{ term: '', friend: "null"}}
+            enableReinitialize
+            initialValues={{ term: '', friend: String(filter.friend) as FriendFormType}}
             validate={usersSearchFormValidate}
             onSubmit={submit}>{({ isSubmitting }) => (
             <Form>
